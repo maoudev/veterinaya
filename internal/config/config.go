9d2c3d5e39b6aa2af0d8954943baf92d9e9a2255
maoudev <renatocavieres4321@gmail.com>
2023-10-28 01:52:34 -0300

    First Commit

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,7 +5,7 @@ import "os"
 var (
 	DB_URL = os.Getenv("DB_URL") // DATABASE's URL.
 
-	API_PORT = os.Getenv("API_PORT") // API PORT.
+	API_PORT = os.Getenv("PORT") // API PORT.
 
 	SECRET_KEY = os.Getenv("SECRET_KEY") // JWT SECRET KEY.
 
